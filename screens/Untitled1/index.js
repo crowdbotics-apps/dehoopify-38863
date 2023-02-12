7cdd455860f38fe4fe629f2fd0709c878865c509
Jason Pyne <jason.pyne@gmail.com>
2023-02-12 09:23:37 -0500

    Crowdbotics Studio: Update app screens

--- screens/Untitled1/index.js
+++ screens/Untitled1/index.js
@@ -1,3 +1,4 @@
+import { ImageBackground } from "react-native";
 import React from "react";
 import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
 
@@ -8,7 +9,7 @@ const Untitled1 = () => {
       padding: 10,
       position: 'relative',
       flex: 1
-    }}></ScrollView>
+    }}><ImageBackground style={styles.yKVXMsvj} source={require("./dehoopify-wht-400x400.png")} resizeMode="cover"></ImageBackground></ScrollView>
     </SafeAreaView>;
 };
 
@@ -22,6 +23,10 @@ const styles = StyleSheet.create({
     backgroundColor: "#E4E4E4",
     borderRadius: 0,
     color: "#777777"
+  },
+  yKVXMsvj: {
+    width: 292,
+    height: 204
   }
 });
 export default Untitled1;
